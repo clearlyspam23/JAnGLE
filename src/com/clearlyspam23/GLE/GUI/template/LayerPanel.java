d009af4a8e76de73b050465da5d09148acc7f699
John Ader <clearlyspam23@gmail.com>
2014-05-23 00:18:43 -0700

    fleshed out the LayerPanel

--- src/com/clearlyspam23/GLE/GUI/template/LayerPanel.java
+++ src/com/clearlyspam23/GLE/GUI/template/LayerPanel.java
@@ -4,8 +4,13 @@ import javax.swing.JPanel;
 import javax.swing.JLabel;
 import java.awt.Font;
 import javax.swing.JList;
+import javax.swing.JTextField;
+import javax.swing.JComboBox;
+import javax.swing.JScrollPane;
+import javax.swing.JButton;
 
 public class LayerPanel extends JPanel {
+	private JTextField textField;
 
 	/**
 	 * Create the panel.
@@ -15,12 +20,49 @@ public class LayerPanel extends JPanel {
 		
 		JLabel lblNewLabel = new JLabel("Layers");
 		lblNewLabel.setFont(new Font("Tahoma", Font.PLAIN, 14));
-		lblNewLabel.setBounds(10, 11, 65, 17);
+		lblNewLabel.setBounds(20, 11, 65, 17);
 		add(lblNewLabel);
 		
 		JList list = new JList();
-		list.setBounds(20, 39, 156, 456);
+		list.setBounds(20, 39, 156, 388);
 		add(list);
+		
+		JLabel lblName = new JLabel("Name");
+		lblName.setBounds(200, 40, 37, 14);
+		add(lblName);
+		
+		JLabel lblType = new JLabel("Type");
+		lblType.setBounds(200, 65, 46, 14);
+		add(lblType);
+		
+		textField = new JTextField();
+		textField.setBounds(247, 37, 156, 20);
+		add(textField);
+		textField.setColumns(10);
+		
+		JComboBox comboBox = new JComboBox();
+		comboBox.setBounds(247, 62, 156, 20);
+		add(comboBox);
+		
+		JScrollPane scrollPane = new JScrollPane();
+		scrollPane.setBounds(186, 90, 289, 405);
+		add(scrollPane);
+		
+		JButton btnCreate = new JButton("Create");
+		btnCreate.setBounds(20, 438, 72, 23);
+		add(btnCreate);
+		
+		JButton btnNewButton = new JButton("Delete");
+		btnNewButton.setBounds(104, 438, 72, 23);
+		add(btnNewButton);
+		
+		JButton btnUp = new JButton("Up");
+		btnUp.setBounds(20, 472, 72, 23);
+		add(btnUp);
+		
+		JButton btnDown = new JButton("Down");
+		btnDown.setBounds(104, 472, 72, 23);
+		add(btnDown);
 
 	}
 
