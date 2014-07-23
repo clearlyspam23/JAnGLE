64b92d4e267beea0d06f3b8f0f1d3e95c0437c92
John Ader <clearlyspam23@gmail.com>
2014-07-23 00:57:30 -0700

    minor bug fix

--- src/com/clearlyspam23/GLE/util/FloatDocumentFilter.java
+++ src/com/clearlyspam23/GLE/util/FloatDocumentFilter.java
@@ -16,22 +16,24 @@ public class FloatDocumentFilter extends DocumentFilter {
         		fb.insertString(off++, str.substring(0, 1), attr);
         		str = str.substring(1);
         	}
-//	    	int i = str.indexOf('.');
-//	    	if(i>=0&&current.indexOf('.')<0){
-//	    		for(int j = str.indexOf('.', i+1); j >=0; j = str.indexOf('.', i+1))
-//	    			str = str.substring(0, j) + str.substring(j+1);
-//	    		fb.insertString(off, str.substring(0, i).replaceAll("\\D++", "") + '.', attr);
-//	    		str = str.substring(i+1);
-//	    		off+=i+1;
-//	    	}
-//	    	else if(i>=0&&current.indexOf('.')>=0)
-//	    		str = str.replaceAll("\\.", "");
+	    	int i = str.indexOf('.');
+	    	if(i>=0&&current.indexOf('.')<0){
+	    		for(int j = str.indexOf('.', i+1); j >=0; j = str.indexOf('.', i+1))
+	    			str = str.substring(0, j) + str.substring(j+1);
+	    		fb.insertString(off, str.substring(0, i).replaceAll("\\D++", "") + '.', attr);
+	    		str = str.substring(i+1);
+	    		off+=i+1;
+	    	}
+	    	else if(i>=0&&current.indexOf('.')>=0)
+	    		str = str.replaceAll("\\.", "");
         	if("0".equals(current)||"-0".equals(current)){
     	    	fb.remove(current.length()-1, 1);
     	    	off = Math.max(0, off-1);
         	}
+        	fb.insertString(off, str.replaceAll("\\D++", ""), attr);  // remove non-digits
     	}
-        fb.insertString(off, str.replaceAll("\\D++", ""), attr);  // remove non-digits
+		else
+			fb.insertString(off, str, attr);
     } 
     @Override
     public void replace(FilterBypass fb, int off, int len, String str, AttributeSet attr) 
@@ -43,22 +45,24 @@ public class FloatDocumentFilter extends DocumentFilter {
         		fb.insertString(off++, str.substring(0, 1), attr);
         		str = str.substring(1);
         	}
-//	    	int i = str.indexOf('.');
-//	    	if(i>=0&&current.indexOf('.')<0){
-//	    		for(int j = str.indexOf('.', i+1); j >=0; j = str.indexOf('.', i+1))
-//	    			str = str.substring(0, j) + str.substring(j+1);
-//	    		fb.insertString(off, str.substring(0, i).replaceAll("\\D++", "") + '.', attr);
-//	    		str = str.substring(i+1);
-//	    		off+=i+1;
-//	    	}
-//	    	else if(i>=0&&current.indexOf('.')>=0)
-//	    		str = str.replaceAll("\\.", "");
+	    	int i = str.indexOf('.');
+	    	if(i>=0&&current.indexOf('.')<0){
+	    		for(int j = str.indexOf('.', i+1); j >=0; j = str.indexOf('.', i+1))
+	    			str = str.substring(0, j) + str.substring(j+1);
+	    		fb.insertString(off, str.substring(0, i).replaceAll("\\D++", "") + '.', attr);
+	    		str = str.substring(i+1);
+	    		off+=i+1;
+	    	}
+	    	else if(i>=0&&current.indexOf('.')>=0)
+	    		str = str.replaceAll("\\.", "");
         	if("0".equals(current)||"-0".equals(current)){
     	    	fb.remove(current.length()-1, 1);
     	    	off = Math.max(0, off-1);
         	}
+        	fb.replace(off, len, str.replaceAll("\\D++", ""), attr);  // remove non-digits
     	}
-    	fb.replace(off, len, str.replaceAll("\\D++", ""), attr);  // remove non-digits
+    	else
+    		fb.replace(off, len, str, attr);
     }
 
 }
