46a6c5c352317d475f8c5b93a44d6640f52159b6
clearlyspam23 <clearlyspam23@gmail.com>
2014-09-17 01:45:04 -0700

    allowed layers visibility to be toggled
    probably took me a few weeks longer then it should have, but eh

--- src/com/clearlyspam23/GLE/GUI/util/PanelList.java
+++ src/com/clearlyspam23/GLE/GUI/util/PanelList.java
@@ -5,39 +5,104 @@ import java.awt.Color;
 import java.awt.Component;
 import java.awt.Dimension;
 import java.awt.EventQueue;
-import java.awt.GridBagConstraints;
-import java.awt.GridBagLayout;
 import java.awt.GridLayout;
+import java.awt.Image;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.event.FocusEvent;
 import java.awt.event.FocusListener;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
+import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
 
-import javax.swing.JButton;
+import javax.imageio.ImageIO;
+import javax.swing.Icon;
+import javax.swing.ImageIcon;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
-import javax.swing.JList;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
+import javax.swing.JToggleButton;
+import javax.swing.UIManager;
+import javax.swing.UnsupportedLookAndFeelException;
 import javax.swing.border.EmptyBorder;
+import javax.swing.event.ListSelectionEvent;
+import javax.swing.event.ListSelectionListener;
 
 public class PanelList extends JPanel implements FocusListener{
 	
 	public static void main(String[] args){
-		final PanelList pList = new PanelList();
-		JPanel panel = new JPanel();
-		JButton button = new JButton("nothing");
-		panel.add(button);
+		try {
+			UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+		} catch (ClassNotFoundException | InstantiationException
+				| IllegalAccessException | UnsupportedLookAndFeelException e1) {
+			//honestly, if this doesnt work, whatever we'll use default. should fail silently.
+		}
+		Image image = null;
+		try{
+			image = ImageIO.read(new File("images/VisibilityIcon.png"));
+		}
+		catch(Exception e){
+			e.printStackTrace();
+			return;
+		}
+		final Icon ico = new ImageIcon(image);
+		final PanelList pList = new PanelList(0, 0, 0, 0);
+		final JPanel panel = new JPanel();
+		panel.setBorder(new EmptyBorder(5, 5, 5, 5));
+		panel.setLayout(new BorderLayout(5, 0));
+		final JToggleButton button = new PushButton(ico);
+		button.setPreferredSize(new Dimension(32, 32));
+		panel.add(button, BorderLayout.WEST);
+		button.addMouseListener(new MouseListener(){
+
+			@Override
+			public void mouseClicked(MouseEvent e) {
+			}
+
+			@Override
+			public void mousePressed(MouseEvent e) {
+				pList.repaint();
+				System.out.println("painting");
+				
+			}
+
+			@Override
+			public void mouseReleased(MouseEvent e) {
+				pList.repaint();
+			}
+
+			@Override
+			public void mouseEntered(MouseEvent e) {
+				pList.repaint();
+				
+			}
+
+			@Override
+			public void mouseExited(MouseEvent e) {
+				pList.repaint();
+			}
+			
+		});
+		button.addActionListener(new ActionListener(){
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				System.out.println("action!");
+				System.out.println(button.isSelected());
+			}
+			
+		});
 		JLabel test = new JLabel("test1");
 		test.addMouseListener(pList.getMouseListener(panel));
 		test.setBackground(new Color(0, 0, 0, 0));
-		panel.add(test);
+		panel.add(test, BorderLayout.CENTER);
 		pList.addPanel(panel);
 		pList.addComponent(new JLabel("test2"));
 		pList.addComponent(new JLabel("test3"));
-		final JList<String> list = new JList<String>(new String[]{"test1", "test2", "test3"});
+		//final JList<String> jList = new JList<String>(new String[]{"test1", "test2", "test3"});
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
 				try {
@@ -72,10 +137,15 @@ public class PanelList extends JPanel implements FocusListener{
 	
 	private int selectionMode;
 	
-	private Color selectionColor = Color.BLUE;
-	private Color panelColor = new Color(0, 0, 0, 0);
+//	UIDefaults defaults = javax.swing.UIManager.getDefaults();
+//	defaults.getColor("List.selectionBackground");
+	
+	private Color selectionColor = javax.swing.UIManager.getDefaults().getColor("List.selectionBackground");
+	private Color panelColor = javax.swing.UIManager.getDefaults().getColor("List.background");
 	private Color outOfFocusColor = Color.LIGHT_GRAY;
-	private static final Color DEFAULT_BACKGROUND = Color.WHITE;
+	private static final Color DEFAULT_BACKGROUND = javax.swing.UIManager.getDefaults().getColor("List.background");
+	
+	private ArrayList<ListSelectionListener> listeners = new ArrayList<ListSelectionListener>();
 
 	/**
 	 * Create the panel.
@@ -84,11 +154,19 @@ public class PanelList extends JPanel implements FocusListener{
 		this(new ArrayList<Component>(), SINGLE_SELECTION);
 	}
 	
+	public PanelList(int insetTop, int insetLeft, int insetBottom, int insetRight){
+		this(new ArrayList<Component>(), SINGLE_SELECTION, insetTop, insetLeft, insetBottom, insetRight);
+	}
+	
 	public PanelList(List<Component> components){
 		this(components, SINGLE_SELECTION);
 	}
 	
 	public PanelList(List<Component> components, int selectionMode){
+		this(components, selectionMode, 0, 5, 0, 5);
+	}
+	
+	public PanelList(List<Component> components, int selectionMode, int insetTop, int insetLeft, int insetBottom, int insetRight){
 		setLayout(new BorderLayout());
 		JPanel temp = new JPanel();
 		temp.setBackground(DEFAULT_BACKGROUND);
@@ -96,7 +174,7 @@ public class PanelList extends JPanel implements FocusListener{
 		add(subPanel, BorderLayout.NORTH);
 		subPanel.setLayout(new GridLayout(0, 1, 0, 0));
 		setBackground(DEFAULT_BACKGROUND);
-		subPanel.setBorder(new EmptyBorder(0, 5, 0, 5));
+		subPanel.setBorder(new EmptyBorder(insetTop, insetLeft, insetBottom, insetRight));
 		subPanel.setBackground(DEFAULT_BACKGROUND);
 		subPanel.setAlignmentX(Component.LEFT_ALIGNMENT);
 		if(!isValidSelectionMode(selectionMode))
@@ -118,20 +196,42 @@ public class PanelList extends JPanel implements FocusListener{
 		if(!panels.contains(panel))
 			throw new IllegalArgumentException("cannot select a panel not in the list");
 		if(selectionMode==SINGLE_SELECTION)
-			clearAllSelections();
+			clearAllSelections(true);
 		selectPanel(panel);
+		int min = 0;
+		int max = panels.size();
+		for(JPanel p : selectedPanels){
+			min = Math.min(panels.indexOf(p), min);
+			max = Math.max(panels.indexOf(p), max);
+		}
+		ListSelectionEvent e = new ListSelectionEvent(this, min, max, false);
+		for(ListSelectionListener l : listeners){
+			l.valueChanged(e);
+		}
 	}
 	
 	public void clearAllSelections(){
-		for(JPanel panel : selectedPanels){
-			panel.setBackground(panelColor);
-		}
-		selectedPanels.clear();
+		clearAllSelections(false);
 	}
 	
 	public void clearSelection(JPanel panel){
 		if(selectedPanels.remove(panel))
 			panel.setBackground(panelColor);
+	}
+	
+	private void clearAllSelections(boolean isAdjusting){
+		int min = 0;
+		int max = panels.size();
+		for(JPanel panel : selectedPanels){
+			panel.setBackground(panelColor);
+			min = Math.min(panels.indexOf(panel), min);
+			max = Math.max(panels.indexOf(panel), max);
+		}
+		selectedPanels.clear();
+		ListSelectionEvent e = new ListSelectionEvent(this, min, max, isAdjusting);
+		for(ListSelectionListener l : listeners){
+			l.valueChanged(e);
+		}
 	}
 	
 	private void selectPanel(JPanel panel){
@@ -214,6 +314,10 @@ public class PanelList extends JPanel implements FocusListener{
 		addPanel(panel);
 	}
 	
+	public void setInsets(int top, int left, int bottom, int right){
+		subPanel.setBorder(new EmptyBorder(top, left, bottom, right));
+	}
+	
 	public MouseListener getMouseListener(final JPanel panel){
 		return new MouseListener(){
 
@@ -282,6 +386,10 @@ public class PanelList extends JPanel implements FocusListener{
 	
 	public Dimension getMaximumSize(){
 		return getPreferredSize();
+	}
+	
+	public void addListSelectionListener(ListSelectionListener listener){
+		listeners.add(listener);
 	}
 
 }
