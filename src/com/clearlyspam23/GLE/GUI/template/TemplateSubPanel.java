0bebf42badf98f6b7e2e9c86691c269c24ecc27f
clearlyspam23 <clearlyspam23@gmail.com>
2014-10-14 01:42:57 -0700

    minor changes
    and comments
    dont tell anyone

--- src/com/clearlyspam23/GLE/GUI/template/TemplateSubPanel.java
+++ src/com/clearlyspam23/GLE/GUI/template/TemplateSubPanel.java
@@ -19,7 +19,22 @@ public abstract class TemplateSubPanel extends NamedPanel{
 		this.pluginManager = pluginManager;
 	}
 	
+	/**
+	 * called before the template is finalized, in order to ensure that any preconditions are met
+	 * this method should return a String for everything that is "wrong" with this panel
+	 * if everything is ok, this method should return either an empty List, or null
+	 * @return a List of Strings with 1 String for every error with this panel
+	 */
 	public abstract List<String> verify();
+	
+	/**
+	 * optional method, called after verify. This method should be used to issue warnings prior to template finalization.
+	 * Acceptable usage would be to pop up a JOptionDialog, verifying that it is ok for a file to be overwritten
+	 * @return true if it is ok to continue with template creation, false if not.
+	 */
+	public boolean getWarnings(){
+		return true;
+	}
 	
 	public abstract void setToTemplate(Template template);
 	
