9525bf7f52d0fc35489bb5a119e5dd4c486ceb60
John Ader <clearlyspam23@gmail.com>
2015-01-19 03:13:45 -0800

    Refactored the Flood Fill tool to use newer abstractions

--- src/com/clearlyspam23/GLE/basic/layers/tile/edit/FloodFillTileCommand.java
+++ src/com/clearlyspam23/GLE/basic/layers/tile/edit/FloodFillTileCommand.java
@@ -7,7 +7,6 @@ import org.piccolo2d.PCamera;
 import org.piccolo2d.event.PInputEvent;
 
 import com.clearlyspam23.GLE.basic.layers.tile.TileData;
-import com.clearlyspam23.GLE.basic.layers.tile.TilesetHandle;
 import com.clearlyspam23.GLE.basic.layers.tile.edit.commands.PlaceTileAction;
 import com.clearlyspam23.GLE.basic.layers.tile.gui.TileLayerPNode;
 import com.clearlyspam23.GLE.basic.layers.tile.gui.TilePNode;
@@ -24,7 +23,7 @@ public class FloodFillTileCommand extends TileDragCommand {
 	
 	protected void setTile(TilePNode tile, PCamera cam){
 //		tile.setImage((Image)null);
-		floodFill(tile, tile.getTileset(), tile.getTilesetX(), tile.getTilesetY(), ((TileLayerPNode)tile.getParent()).getNodeGrid());
+		floodFill(tile, tile.getTileData(), ((TileLayerPNode)tile.getParent()).getNodeGrid());
 //		tile.invalidatePaint();
 	}
 	
@@ -36,14 +35,9 @@ public class FloodFillTileCommand extends TileDragCommand {
 		return false;
 	}
 	
-	private boolean isSameTile(TilesetHandle handle1, int x1, int y1, TilesetHandle handle2, int x2, int y2){
-		return handle1==handle2&&x1==x2&&y1==y2;
-	}
 	
-	private void floodFill(TilePNode node, TilesetHandle target, int targetX, int targetY, TilePNode[][] grid){
-//		if(isSameTile(target, targetX, targetY, data.getCurrentTileset(), data.getSelectedX(), data.getSelectedY()))
-//			return;
-		if(!isSameTile(target, targetX, targetY, node.getTileset(), node.getTilesetX(), node.getTilesetY()))
+	private void floodFill(TilePNode node, TileData target, TilePNode[][] grid){
+		if(!target.equals(node.getTileData()))
 			return;
 		Pair<TilePNode, TileData> pair = new Pair<TilePNode, TileData>(node, node.getTileData());
 		if(!node.setTileset(data.getCurrentTileset(), data.getSelectedX(), data.getSelectedY()))
@@ -52,13 +46,13 @@ public class FloodFillTileCommand extends TileDragCommand {
 		int x = node.getGridX();
 		int y = node.getGridY();
 		if(x-1>=0)
-			floodFill(grid[x-1][y], target, targetX, targetY, grid);
+			floodFill(grid[x-1][y], target, grid);
 		if(x+1<grid.length)
-			floodFill(grid[x+1][y], target, targetX, targetY, grid);
+			floodFill(grid[x+1][y], target, grid);
 		if(y-1>=0)
-			floodFill(grid[x][y-1], target, targetX, targetY, grid);
+			floodFill(grid[x][y-1], target, grid);
 		if(y+1<grid[x].length)
-			floodFill(grid[x][y+1], target, targetX, targetY, grid);
+			floodFill(grid[x][y+1], target, grid);
 	}
 
 	@Override
